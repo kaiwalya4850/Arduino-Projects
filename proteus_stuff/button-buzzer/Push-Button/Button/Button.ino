bafb4af1bf030ba69b0ae1203446ecf6500bd54c
Kaiwalya Patil <39062805+kaiwalya4850@users.noreply.github.com>
2021-03-31 13:14:20 +0530

    Code, circuit diagram and simulation files for Buzzer

---
+++ proteus_stuff/button-buzzer/Push-Button/Button/Button.ino
@@ -0,0 +1,32 @@
+const int buttonPin = 0;     // the number of the pushbutton pin
+const int ledPin1 = 1;      // the number of the LED pin
+const int ledPin2 = 2;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin1, OUTPUT);   
+  pinMode(ledPin2, OUTPUT);   
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);     
+}
+
+void loop(){
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {     
+    // turn LED on:    
+    digitalWrite(ledPin1, HIGH);
+    digitalWrite(ledPin2, HIGH);   
+  } 
+  else {
+    // turn LED off:
+    digitalWrite(ledPin1, LOW); 
+    digitalWrite(ledPin2, LOW); 
+  }
+}
